faa283cf5ce90cd7ab7a9d0c68c9228785ddc61f
mamerida <mamerida2018@gmail.com>
2024-01-16 17:21:13 -0300

    add modules to proyect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { VideosModule } from './videos/videos.module';
+import { AwardsModule } from './awards/awards.module';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, VideosModule, AwardsModule, CourseModule],
   controllers: [AppController],
   providers: [AppService],
 })
